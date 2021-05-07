5d4b61b18ee49e060fb3d0d69b470b723abdb3a2
leandrofabianjr <leandrofabianjr@gmail.com>
2021-05-07 18:06:09 -0300

    Implementado autenticação seguindo o tutorial https://dev.to/nestjs/authentication-and-sessions-for-mvc-apps-with-nestjs-55a4

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,50 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Render,
+  Request,
+  Res,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { AuthExceptionFilter } from './common/filters/auth-exceptions.filter';
+import { AuthenticatedGuard } from './common/guards/authenticated.guard';
+import { LoginGuard } from './common/guards/login.guard';
 
+@UseFilters(AuthExceptionFilter)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @Render('index')
+  @Get('/')
+  @Render('login')
   index() {
-    const title = 'Controle';
-    const message = this.appService.getHello();
-    return { title, message };
+    return;
+  }
+
+  @UseGuards(LoginGuard)
+  @Post('/login')
+  login(@Res() res: Response): void {
+    res.redirect('/home');
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('/home')
+  @Render('home')
+  getHome() {
+    return;
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('/profile')
+  @Render('profile')
+  getProfile() {
+    return;
+  }
+
+  @Get('/logout')
+  logout(@Request() req, @Res() res: Response): void {
+    req.logout();
+    res.redirect('/');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as exphbs from 'express-handlebars';
+import * as passport from 'passport';
+import session = require('express-session');
+import flash = require('connect-flash');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('ejs');
+  app.setViewEngine('hbs');
+
+  app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
+
+  app.use(
+    session({
+      secret: 'very secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
 
   await app.listen(3000);
 }
